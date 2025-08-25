77681ed167a3924fd139bacd5ec4194209a4a1d0
RachnaSree <rachanasreeuppula@gmail.com>
2025-08-25 14:57:15 +0530

    Created main.tf for ec2 provision

---
+++ main.tf
@@ -0,0 +1,31 @@
+# Provider
+provider "aws" {
+  region = "us-east-1"
+}
+
+# Get the latest Amazon Linux 2 AMI
+data "aws_ami" "amazon_linux" {
+  most_recent = true
+  owners      = ["amazon"]
+
+  filter {
+    name   = "name"
+    values = ["amzn2-ami-hvm-*-x86_64-gp2"]
+  }
+}
+
+# Variable for instance names
+variable "instance_names" {
+  default = ["app-server", "db-server"]
+}
+
+# EC2 Instances
+resource "aws_instance" "ec2_instances" {
+  for_each      = toset(var.instance_names) # loop through instance names
+  ami           = data.aws_ami.amazon_linux.id
+  instance_type = "t2.micro"
+
+  tags = {
+    Name = "${each.key}-${terraform.workspace}" # dynamic name
+  }
+}
